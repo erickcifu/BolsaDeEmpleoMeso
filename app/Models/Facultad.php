f0d5b215ccc9af040f449d3dc5ea53e755f78de6
Antuan92 <ajlb4@hotmail.com>
2023-09-11 20:31:10 -0600

    tablas corregidas

--- app/Models/Facultad.php
+++ app/Models/Facultad.php
@@ -2,38 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-/**
- * Class Facultade
- *
- * @property $id
- * @property $facultad
- * @property $EstadoFacultad
- * @property $created_at
- * @property $updated_at
- *
- * @package App
- * @mixin \Illuminate\Database\Eloquent\Builder
- */
 
-class Facultad extends Model
+class facultad extends Model
 {
-    static $rules = [
-		'facultad' => 'required',
-        'EstadoFacultad' => 'required',
-    ];
-
-    protected $perPage = 20;
-
-    /**
-     * Attributes that should be mass-assignable.
-     *
-     * @var array
-     */
-    protected $fillable = ['facultad', 'EstadoFacultad'];
-
-    public function ofertas()
-    {
-        return $this->hasMany('App\Models\oferta', 'facultad_id', 'id');
-    }
+    use HasFactory;
 }
