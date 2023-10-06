ac12c3ef5c7d3401172851bbaff1057984910e3a
Antuan92 <ajlb4@hotmail.com>
2023-10-05 22:48:22 -0600

    Merge branch 'master' of https://github.com/erickcifu/BolsaDeEmpleoMeso

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\OfertasEstudiantes;
+use App\Http\Livewire\Ofertas;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -23,16 +24,23 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 //Route Hooks - Do not delete//
+<<<<<<< HEAD
 	Route::view('empresas', 'livewire.empresas.index')->middleware('auth');
 	Route::view('municipios', 'livewire.municipios.index')->middleware('auth');
 	Route::view('departamentos', 'livewire.departamentos.index')->middleware('auth');
 	Route::view('cartarecomendacions', 'livewire.cartarecomendacions.index')->middleware('auth');
 	Route::view('autoridadacademicas', 'livewire.autoridadacademicas.index')->middleware('auth');
+=======
+	Route::view('estudiantes', 'livewire.estudiantes.index')->middleware('auth');
+	Route::view('carreras', 'livewire.carreras.index')->middleware('auth');
+	Route::view('facultads', 'livewire.facultads.index')->middleware('auth');
+	Route::get('/formulario/create', [Ofertas::class, 'redirectToFormularioCreate'])->name('formulario.create')->middleware('auth');
+>>>>>>> a1557ec8fb6e68bae2024f4d30b07838dcbcc5e1
 	//Route::get('/ofertasestudiantes', OfertasEstudiantes::class)->middleware('auth');
 	Route::view('ofertasestudiantes', 'livewire.ofertasestudiantes.index')->middleware('auth');
 	Route::view('entrevistas', 'livewire.entrevistas.index')->middleware('auth');
 	Route::view('postulacions', 'livewire.postulacions.index')->middleware('auth');
-	Route::view('ofertas', 'livewire.ofertas.index')->middleware('auth');
+	Route::view('ofertas', 'livewire.ofertas.index')->name('ofertas')->middleware('auth');
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
