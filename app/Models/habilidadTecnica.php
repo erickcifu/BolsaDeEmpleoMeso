6ec572288bdb86f9628106c87025e22e8c26eec1
erickcifu <erickalfred29@gmail.com>
2024-01-11 12:39:15 -0600

    Cambios en logo y habilidad Tecnica

--- app/Models/habilidadTecnica.php
+++ app/Models/habilidadTecnica.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Habilidadtecnica extends Model
+class habilidadTecnica extends Model
 {
 	use HasFactory;
     protected $primaryKey = 'tecnicaId';
