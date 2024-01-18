4f4208639d3fd7ef3c75ec53ebf72061ff236336
mafer85 <mendozamafer2000@gmail.com>
2024-01-18 02:26:17 -0600

    Carta de recomendaión

--- resources/views/livewire/cartarecomendacions/viewpdf.blade.php
+++ resources/views/livewire/cartarecomendacions/viewpdf.blade.php
@@ -14,64 +14,67 @@
 
 <body>
   	@foreach ($ccartarecomendacions as $item)
-	@if ( $item->estudiante->user_id==$usuario)
-	<p></p>
-	<p></p>
-	<p></p>
-	
-	<li class="list-unstyled">
-		<h6>
+			@if ( $item->estudiante->user_id==$usuario)
+			@php
+                $cartaEncontrada = true;
+            @endphp
+			<p></p>
+			<p></p>
+			<p></p>
+			
+			<li class="list-unstyled">
+				<h6>
+				
+				</h6>
+			</li>
+			<p></p>
+			<p>Quetzaltenango, Guatemala. </p>
+			<p></p>
+			<br>
+			<br>
+			
+			<br>
+			<br>
+			<br>
+			<p><b>A quién pueda interesar: </b></p>
+			<br>
+			<p class="text-justify">
+
+			A través de la presente, extiendo mi recomendación personal en apoyo del estudiante
+			<b> {{ $item->estudiante->nombre }} {{ $item->estudiante->apellidos}} </b>, quién se identifica con número 
+			de carné <b>{{$item->estudiante->carnet}}</b>, que se destaca por <b>{{ $item->cargoYTareasRealizadas }} </b>.  
+			</p>
+			<p></p>
+			<p></p>
+			<p></p>
+			
+
+		<p class="text-justify">
+		Y para los usos que al  interesado convenga, extiendola presente en la fecha {{ date('d/m/Y', strtotime($item->fechaCarta)) }}. 
 		
-		</h6>
-	</li>
-	<p></p>
-	<p>Quetzaltenango, Guatemala. {{ $item->fechaCarta }} </p>
-	<p></p>
-	<br>
-	<br>
-	
-	<br>
-	<br>
-	<br>
-	<p><b>A quién pueda interesar: </b></p>
-	<br>
-	<p class="text-justify">
+		</p>
 
-	A través de la presente, extiendo mi recomendación personal en apoyo del estudiante
-	<b> {{ $item->estudiante->nombre }} {{ $item->estudiante->apellidos}} </b>, quién se identifica con número 
-	de carné <b>{{$item->estudiante->carnet}} </b>, que se destaca por <b>{{ $item->cargoYTareasRealizadas }} </b>.  
-	</p>
-	<p></p>
-	<p></p>
-	<p></p>
-	
-
-<p class="text-justify">
-Y para los usos que al  interesado convenga, extiendola presente en la fecha {{ date('d/m/Y', strtotime($item->fechaCarta)) }}. 
- 
-</p>
-
-<br>
-<br>
-<br>
-<p>Atentamente,   </p>
-<br>
-<br>
-<p class="text-center">
-<img src="{{$item->firmaAutoridad}}" alt="" width="100" height="100"/>
-</p>
-<p class="text-center">{{$item->autoridadAcademica->nombreAutoridad}}  {{$item->autoridadAcademica->apellidosAutoridad}}</p>
-<p class="text-center"> cel: {{$item->telefonoAutoridad}}</p>
-<br>
-<br>
-<br><br>
-<br>
-<br>	
-	@else
-	<h2 class="text-center">no existe carta</h2>
-		
-	@endif
+		<br>
+		<br>
+		<br>
+		<p>Atentamente,   </p>
+		<br>
+		<br>
+		<p class="text-center">
+		<img src="{{$item->firmaAutoridad}}" alt="" width="100" height="100"/>
+		</p>
+		<p class="text-center">{{$item->autoridadAcademica->nombreAutoridad}}  {{$item->autoridadAcademica->apellidosAutoridad}}</p>
+		<p class="text-center"> cel: {{$item->telefonoAutoridad}}</p>
+		<br>
+		<br>
+		<br><br>
+		<br>
+		<br>
 	@endforeach
+	@if (!$cartaEncontrada)
+        <h2 class="text-center">No existe carta</h2>
+    @endif
+	
 
 </body>
 
