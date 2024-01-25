842308db44ff72369e4027ce6d3ba7fb5abc033b
erickcifu <erickalfred29@gmail.com>
2024-01-25 15:04:52 -0600

    cambios autoridades

--- resources/views/livewire/autoridadacademicas/view.blade.php
+++ resources/views/livewire/autoridadacademicas/view.blade.php
@@ -19,67 +19,60 @@
 						</div>
 					</div>
 				</div>
-					
 					<div class="card-body">
 							@include('livewire.autoridadacademicas.modals')
-					<div class="table-responsive">
-						<table class="table table-bordered table-sm">
-							<thead class="thead">
-								<tr> 
-									<td style="background-color: #005c35;"><b style="color: #f0eadc;">#</b></td> 
-									<th style="background-color: #005c35;"><b style="color: #f0eadc;">Nombre</b></th>
-									<th style="background-color: #005c35;"><b style="color: #f0eadc;">Apellido</b></th>
-									<th style="background-color: #005c35;"><b style="color: #f0eadc;">Facultad</b></th>
-									<th style="background-color: #005c35;"><b style="color: #f0eadc;">Estado</b></th>
-								
-									<td style="background-color: #005c35;"><b style="color: #f0eadc;"> Acciones</b></td>
-								</tr>
-							</thead>
-							<tbody>
-								@forelse($autoridadacademicas as $row)
-								<tr>
-									<td>{{ $loop->iteration }}</td> 
-									<td>{{ $row->nombreAutoridad }}</td>
-									<td>{{ $row->apellidosAutoridad }}</td>
-									<td>{{ $row->facultad->Nfacultad}}</td>
-									
-									<td>
-										<?php if ($row->estadoAutoridad == 1): ?>
-											<span class="badge" style="background-color: #005c35;"><b>Activo</b></span>
-											<a data-bs-toggle="modal" data-bs-target="#EliminarDataModal" class="dropdown-item" wire:click="edit2({{$row->autoridadId}})"><i class="fa fa-trash"></i> Desactivar </a>	
-										<?php else: ?>
-											<span class="badge" style="background-color: #d3d3d3;"><b style="color: black;">Inactivo</b></span>
-											<a data-bs-toggle="modal" data-bs-target="#ActivarDataModal" class="dropdown-item" wire:click="edit2({{$row->autoridadId}})"><i class="fa fa-edit"></i> Activar </a>
-										<?php endif; ?>
-											
-									</td>
-									
-
-									
-									<td width="125">
-										<div class="dropdown">
-											
-											
-											<a data-bs-toggle="modal" data-bs-target="#updateDataModal" class="dropdown-item" wire:click="edit({{$row->autoridadId}})"><i class="fa fa-edit"></i> Editar </a>
-											<a data-bs-toggle="modal" data-bs-target="#DeletDataModal" class="dropdown-item" wire:click="eliminar({{$row->autoridadId}})"><i class="fa fa-trash"></i> Eliminar Registro </a>
-											
-											{{-- boton que cambie para que no usemos el destroy --}}
-										{{--<a data-bs-toggle="modal" data-bs-target="#EliminarDataModal" class="dropdown-item" wire:click="edit2({{$row->autoridadId}})"><i class="fa fa-trash"></i> Desactivar </a>	--}}
-										
-									</div>									
-									</td>
-								</tr>
-								@empty
-								<tr>
-									<td class="text-center" colspan="100%">Sin datos</td>
-								</tr>
-								@endforelse
-							</tbody>
-						</table>						
-						<div class="float-end">{{ $autoridadacademicas->links() }}</div>
+						<div class="table-responsive">
+								<table class="table table-bordered table-sm">
+									<thead class="thead">
+										<tr> 
+											<td style="background-color: #005c35;"><b style="color: #f0eadc;">#</b></td> 
+											<th style="background-color: #005c35;"><b style="color: #f0eadc;">Nombre</b></th>
+											<th style="background-color: #005c35;"><b style="color: #f0eadc;">Apellido</b></th>
+											<th style="background-color: #005c35;"><b style="color: #f0eadc;">Facultad</b></th>
+											<th style="background-color: #005c35;"><b style="color: #f0eadc;">Estado</b></th>
+											<td style="background-color: #005c35;"><b style="color: #f0eadc;"> Acciones</b></td>
+										</tr>
+									</thead>
+									<tbody>
+										@forelse($autoridadacademicas as $row)
+										<tr>
+											<td>{{ $loop->iteration }}</td> 
+											<td>{{ $row->nombreAutoridad }}</td>
+											<td>{{ $row->apellidosAutoridad }}</td>
+											<td>{{ $row->facultad->Nfacultad}}</td>
+											<td>
+												<?php if ($row->estadoAutoridad == 1): ?>
+													<span class="badge" style="background-color: #005c35;"><b>Activo</b></span>
+													<a data-bs-toggle="modal" data-bs-target="#EliminarDataModal" class="dropdown-item" wire:click="edit2({{$row->autoridadId}})"><i class="fa fa-trash"></i> Desactivar </a>	
+												<?php else: ?>
+													<span class="badge" style="background-color: #d3d3d3;"><b style="color: black;">Inactivo</b></span>
+													<a data-bs-toggle="modal" data-bs-target="#ActivarDataModal" class="dropdown-item" wire:click="edit2({{$row->autoridadId}})"><i class="fa fa-edit"></i> Activar </a>
+												<?php endif; ?>	
+											</td>
+											<td width="125">
+												<div class="dropdown">
+													
+													
+													<a data-bs-toggle="modal" data-bs-target="#updateDataModal" class="dropdown-item" wire:click="edit({{$row->autoridadId}})"><i class="fa fa-edit"></i> Editar </a>
+													<a data-bs-toggle="modal" data-bs-target="#DeletDataModal" class="dropdown-item" wire:click="eliminar({{$row->autoridadId}})"><i class="fa fa-trash"></i> Eliminar Registro </a>
+													
+													{{-- boton que cambie para que no usemos el destroy --}}
+												{{--<a data-bs-toggle="modal" data-bs-target="#EliminarDataModal" class="dropdown-item" wire:click="edit2({{$row->autoridadId}})"><i class="fa fa-trash"></i> Desactivar </a>	--}}
+												
+											</div>									
+											</td>
+										</tr>
+										@empty
+										<tr>
+											<td class="text-center" colspan="100%">Sin datos</td>
+										</tr>
+										@endforelse
+									</tbody>
+								</table>						
+								<div class="float-end">{{ $autoridadacademicas->links() }}
+								</div>
 						</div>
-					</div>
-				</div>
+					</div>	
 			</div>	
 		</div>
 	</div>
